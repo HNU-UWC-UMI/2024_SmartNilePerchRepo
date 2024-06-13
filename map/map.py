60f42defa5d17cd76b90405749bbb6fa5e63bae5
Jan Schneider <j.schn@tutanota.com>
2024-06-13 11:13:00 +0200

    deletion

--- map/map.py
+++ map/map.py
@@ -1,14 +1,47 @@
+import json
 import folium
 from folium.plugins import MarkerCluster
 import pandas as pd
 import webbrowser
 
-# Ulm
-# latitude + longitude
-location = [48.39841000, 9.99155000]
+json_file = "geodata_mocking.json"
 
-my_map = folium.Map(location=location, zoom_start=13)
+
+def get_request():
+    # implement request and store response as json
+    pass
+
+
+def get_geolocation(param: str):
+    # parse json with open and give it an argument
+    pass
+
+
+def set_up_map():
+    # Ulm
+    # latitude + longitude
+    country_overview = [48.39841000, 9.99155000]
+    # initial creation of the map
+    return folium.Map(location=country_overview, zoom_start=13)
+
+
+def set_markers(map):
+    # implement json
+    with open(json_file) as jason_data:
+        data = json.loads(jason_data)
+
+    print(data)
+    """
+    for mark in markers:
+        folium.Marker(mark, popup="CU Boulder").add_to(map)
+
+    return map
+    """
+
+
+overview = set_up_map()
+map = set_up_map(overview)
 
 # Display the map
-my_map.save("test.html")
+map.save("test.html")
 webbrowser.open("test.html")
